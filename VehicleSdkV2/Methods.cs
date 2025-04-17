5484ee7823aebd68a0339e0e743b1111702462e5
breece-pingle-sl <breece.pingle@safelite.com>
2025-04-17 11:05:22 -0400

    package test

---
+++ VehicleSdkV2/Methods.cs
@@ -0,0 +1,14 @@
+﻿using VehicleSdkV2.Models.Response;
+
+namespace VehicleSdkV2;
+
+public class Methods
+{
+    public static void printHi() {
+        Console.WriteLine("hi");
+    }
+
+    public VehicleResponseModel LookupByCarId (string carId) {
+        return new VehicleResponseModel();
+    }
+}
---
+++ VehicleSdkV2/Models/Response/VehicleResponseModel.cs
@@ -0,0 +1,24 @@
+namespace VehicleSdkV2.Models.Response;
+
+public class VehicleResponseModel
+{
+    public string CarId { get; set; }
+    public int Year { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string BodyStyle { get; set; }
+    public string VehicleCategory { get; set; }
+    public string PreferredAdasTool { get; set; }
+    public bool IsProblemVehicle { get; set; }
+    public string ImageUrl { get; set; }
+    public string SpecialVehicleCategory { get; set; }
+    public string SpecialVehicleClass { get; set; }
+    public bool IsSpecialVehicle { get; set; }
+    public bool CanSafeliteService { get; set; }
+    public bool HasSplitWindshield { get; set; }
+    public List<string> RecalibrationServices { get; set; }
+    public bool IsMobileStaticRecalibrationApplicable { get; set; }
+    public string VehicleSubType { get; set; }
+    public bool IsAdasVehicle { get; init; }
+    public bool IsBigTruck { get; set; }
+}

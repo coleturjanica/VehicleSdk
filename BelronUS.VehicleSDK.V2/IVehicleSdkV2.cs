e66e560c62b6a25fa8709d37a792162852a107b7
Breece Pingle <breece.pringle@safelite.com>
2025-04-18 10:02:02 -0400

    Feature/v2 lookup by carID (#3)
    * package test
    
    * endpoint test 1
    
    * version increment
    
    * package name update
    
    * change name to match controller
    
    ---------
    
    Co-authored-by: breece-pingle-sl <breece.pingle@safelite.com>

---
+++ BelronUS.VehicleSDK.V2/IVehicleSdkV2.cs
@@ -0,0 +1,9 @@
+﻿using BelronUS.VehicleSDK.V2.Models.Response;
+
+namespace BelronUS.VehicleSDK.V2;
+
+public interface IVehicleSdkV2
+{
+    void printHi();
+    Task<VehicleResponseModel> LookupByCarId(string carId);
+}

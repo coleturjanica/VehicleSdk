0693658099da1b0070ec47acd0aff559d591fd15
breece-pingle-sl <breece.pingle@safelite.com>
2025-04-18 11:09:53 -0400

    beginnings of basesdk package

---
+++ BelronUS.SDK.Base/BaseSdkModels.cs
@@ -0,0 +1,19 @@
+﻿namespace BelronUS.SDK.Base;
+
+internal interface IBaseSdkModel
+{
+    string ApplicationName { get; set; }
+    string CorrelationId { get; set; }
+}
+
+interface IBaseSdkRequest : IBaseSdkModel
+{
+    Dictionary<string, string> Headers { get; set; }
+    string ApiBaseUrl { get; set; }
+}
+
+interface IBaseSdkResponse : IBaseSdkModel
+{
+    Dictionary<string, string> Headers { get; set; }
+    string ApiBaseUrl { get; set; }
+}

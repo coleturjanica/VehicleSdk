49d4da827286c9238f4b30df1c8436ac53efb5f4
breece-pingle-sl <breece.pingle@safelite.com>
2025-04-21 16:29:47 -0400

    add XML contracts

--- BelronUS.VehicleSDKs/V2/Models/Request/LookupByAddressRequestModel.cs
+++ BelronUS.VehicleSDKs/V2/Models/Request/LookupByAddressRequestModel.cs
@@ -2,14 +2,27 @@ using BelronUS.SDK.Base;
 
 namespace BelronUS.VehicleSDKs.V2.Models.Request;
 
+
+/// <summary>
+/// Represents the fields involved in searching for a vehicle by an address.
+/// </summary>
+/// <remarks>
+/// The following fields are required:
+/// <list type="bullet">
+/// <item><description>LastName</description></item>
+/// <item><description>RegisteredStreetAddress</description></item>
+/// <item><description>State</description></item>
+/// <item><description>Zip</description></item>
+/// </list>
+/// </remarks>
 public class LookupByAddressRequestModel : BaseSdkRequest
 {
     public string FirstName { get; set; }
-
+    
     public string LastName { get; set; }
-
+    
     public string RegisteredStreetAddress { get; set; }
-
+    
     public string City { get; set; }
 
     public string State { get; set; }

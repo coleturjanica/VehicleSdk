2b4c4577153bdb2875e760ec3ce1333f2dbb6daf
breece-pingle-sl <breece.pingle@safelite.com>
2025-04-21 15:21:14 -0400

    remove unneeded reflection

--- BelronUS.SDK.Base/BaseSdkHelpers.cs
+++ BelronUS.SDK.Base/BaseSdkHelpers.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Microsoft.AspNetCore.WebUtilities;
+﻿using Microsoft.AspNetCore.WebUtilities;
 
 namespace BelronUS.SDK.Base;
 
@@ -10,7 +9,7 @@ public static class QueryStringHelper
         // Use reflection to get all properties of the request object
         var queryStringDictionary = new Dictionary<string, string>();
 
-        foreach (var property in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance))
+        foreach (var property in typeof(T).GetProperties())
         {
             var value = property.GetValue(request)?.ToString();
             if (!string.IsNullOrEmpty(value))
